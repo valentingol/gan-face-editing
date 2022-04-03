dca40f02bdec369f88036e3c57cd1b245a31412d
Valentin Goldité <valentin.goldite@gmail.com>
2022-04-03 14:25:23 +0200

    :rocket: heavily improve speed of distance computation with scipy.ndimage.distance_transform_edt

--- tools/preprocess/mixup_compute_dist.py
+++ tools/preprocess/mixup_compute_dist.py
@@ -3,15 +3,12 @@ import os
 import cv2
 import numpy as np
 from scipy.spatial.distance import cdist
+from scipy.ndimage import distance_transform_edt as dist_edt
 
 if __name__ == '__main__':
     # domains img: balck and white image (domain is black pixels)
     domains_img_path = './preprocess/mixup/domains/images'
     domains_dist_path = './preprocess/mixup/domains/distances'
-    # Create matrix of coords
-    y, x = np.meshgrid(np.arange(512), np.arange(512))
-    y, x = np.expand_dims(y, axis=-1), np.expand_dims(x, axis=-1)
-    coords = np.concatenate([x, y], axis=-1)  # coords[i, j] = [i, j]
 
     for domain_name in os.listdir(domains_img_path):
         if domain_name.endswith('.png'):
@@ -21,17 +18,8 @@ if __name__ == '__main__':
             domain_y = np.expand_dims(domain_y, axis=-1)
             domain_x = np.expand_dims(domain_x, axis=-1)
             domain = np.concatenate([domain_x, domain_y], axis=-1)
+            domain = np.squeeze(domain)
             domain_basename = os.path.splitext(domain_name)[0]
-
-            # Compute distance between each pixel and the doamin
-            # NOTE; lines are processed per batch to avoid memory error
-            batch_len = 4
-            dist = np.zeros((512, 512))
-            for i in range(0, len(coords), batch_len):
-                coords_i = coords[i: i + batch_len].reshape(-1, 2)
-                dist_i = cdist(coords_i, domain)
-                dist_i = np.min(dist_i, axis=-1)
-                dist[i: i + batch_len] = dist_i.reshape((batch_len, -1))
-            dist = dist.T
+            dist = dist_edt(domain)
             np.save(os.path.join(domains_dist_path, domain_basename + '_dist.npy'), dist)
         print(f'distance to {domain_basename} computed')
