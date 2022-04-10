b23bbc23428774dba3eb4ba7824716063dfa8366
ubar667 <ayoub.rhim@eleves.enpc.fr>
2022-04-10 05:01:11 +0200

    :sparkles: add depth estimation feature

--- tools/pipeline_submission.py
+++ tools/pipeline_submission.py
@@ -4,3 +4,4 @@ if __name__ == '__main__':
     os.system('FORCE_NATIVE=1 python3 tools/translate.py')
     os.system('python3 tools/preprocess/mixup.py')
     os.system('python3 tools/preprocess/segment.py')
+    os.system('python3 tools/preprocess/estimate_depth.py')
