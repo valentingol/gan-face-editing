44aa187845e00faf1fadba8ec32b4f5acefb023a
Valentin Goldité <valentin.goldite@gmail.com>
2022-06-09 22:07:35 +0200

    ✨ allow no using caracteristics  of the image if not provided (by default)

--- pipeline/utils/translation/get_translations.py
+++ pipeline/utils/translation/get_translations.py
@@ -7,51 +7,75 @@ import torch
 def browse_translation_dir(translation_dir, caracs):
     translations = {}
     translations_default = {}
-    for fname in os.listdir(translation_dir):
-        translation_path = os.path.join(translation_dir, fname)
-        basename = fname.split('.')[0]
-        split_name = basename.split('_')
-        char = split_name[0]
-        if len(split_name) == 2:
-            if split_name[1] not in {'min', 'max'}:
-                num = int(split_name[1])
-                if caracs[char] != num:
+
+    # If no translations are provided, skip the browsing
+    if not os.path.exists(translation_dir):
+        return {}
+
+    if caracs is None:  # Apply all transformations
+        for fname in os.listdir(translation_dir):
+            translation_path = os.path.join(translation_dir, fname)
+            transfo = np.load(translation_path)
+            basename = fname.split('.')[0]
+            translations[basename] = torch.tensor(transfo, dtype=torch.float32)
+    else:  # Apply only the transformations that are not already present
+        for fname in os.listdir(translation_dir):
+            translation_path = os.path.join(translation_dir, fname)
+            basename = fname.split('.')[0]
+            split_name = basename.split('_')
+            char = split_name[0]
+            if len(split_name) == 2:
+                if split_name[1] not in {'min', 'max'}:
+                    num = int(split_name[1])
+                    if caracs[char] != num:
+                        transfo = np.load(translation_path)
+                        translations_default[char + '_' + str(num)] \
+                            = torch.tensor(transfo, dtype=torch.float32)
+                else:
+                    degree = split_name[1]
                     transfo = np.load(translation_path)
-                    translations_default[char + '_' + str(num)] = torch.tensor(
+                    translations_default[char + '_' + degree] = torch.tensor(
                         transfo, dtype=torch.float32
                         )
             else:
-                degree = split_name[1]
-                transfo = np.load(translation_path)
-                translations_default[char + '_' + degree] = torch.tensor(
-                    transfo, dtype=torch.float32
-                    )
-        else:
-            if split_name[1] not in {'min', 'max'}:
-                num, from_num = int(split_name[1]), int(split_name[3])
-                if caracs[char] == from_num:
-                    transfo = np.load(translation_path)
-                    translations[char + '_' + str(num)] = torch.tensor(
-                        transfo, dtype=torch.float32
-                        )
-            else:
-                raise NotImplementedError('Specific translation of "cursor '
-                                          'attribute" is not supported yet.')
-    # Overwrite default translations with translations
-    # from specific caracteristics
-    translations = {**translations_default, **translations}
+                if split_name[1] not in {'min', 'max'}:
+                    num, from_num = int(split_name[1]), int(split_name[3])
+                    if caracs[char] == from_num:
+                        transfo = np.load(translation_path)
+                        translations[char + '_' + str(num)] = torch.tensor(
+                            transfo, dtype=torch.float32
+                            )
+                else:
+                    raise NotImplementedError(
+                        'Specific translation (ie. using "fr_...") of "min/max'
+                        f' attribute" is not supported yet. {translation_path}'
+                        ' should be removed.')
+
+        # Overwrite default translations with translations
+        # from specific caracteristics
+        translations = {**translations_default, **translations}
     return translations
 
 
-def get_translations(carac_list, translation_dir, use_precomputed=True):
+def get_translations(translation_dir, carac_list, use_precomputed):
     """ Get translations for a given list of caracteristics.
 
     Parameters
     ----------
-    carac_list : list[int]
-        List of caracteristics (9 int elements).
     translation_dir: str
         Path to the directory containing the translations.
+    carac_list : list[int] or None
+        If not None, carac_list should be aist of caracteristics with 9
+        int elements following the convention presented here:
+        https://transfer-learning.org/rules. Then, the translations
+        will be based on the caracteristics provided. If None,
+        no caracteristics are taken into account and the translations
+        will be always the same (unconditional).
+    use_precomputed : bool
+        If True, use the precomputed translations in
+        'projection/default_translations' and overwrite with
+        tranlsations in translation_dir. If False, compute only
+        the translations in translation_dir.
 
     Returns
     -------
@@ -59,14 +83,25 @@ def get_translations(carac_list, translation_dir, use_precomputed=True):
         Translations for all the changes that are not in the original image
         (key: 'identifier', value: translation).
     """
-    sk, a, se, _, _,  b, hc, d, hs = carac_list
-    caracs = {'Sk': sk, 'A': a, 'Se': se, 'B': b, 'Hc': hc, 'D': d, 'Hs': hs}
+    if carac_list is not None:
+        sk, a, se, _, _,  b, hc, d, hs = carac_list
+        caracs = {'Sk': sk, 'A': a, 'Se': se, 'B': b, 'Hc': hc, 'D': d,
+                  'Hs': hs}
+    else:
+        caracs = None
 
     # Get pre-computed translations
     if use_precomputed:
         precomputed_translations = browse_translation_dir(
-            'projection/default_translations', caracs
+            'projection/default_translations/unconditional', caracs
             )
+        # Add conditional translations only when caracs are given
+        if caracs is not None:
+            precomputed_translations_2 = browse_translation_dir(
+                'projection/default_translations/conditional', caracs
+                )
+            precomputed_translations = {**precomputed_translations,
+                                        **precomputed_translations_2}
     else:
         precomputed_translations = {}
 
@@ -74,21 +109,10 @@ def get_translations(carac_list, translation_dir, use_precomputed=True):
     translations = browse_translation_dir(translation_dir, caracs)
     translations = {**precomputed_translations, **translations}
 
-    # Replace 'Hc_4' by 'bald':
-    if hc != 4:
-        translation_bald = translations.pop('Hc_4')
-        translations['bald'] = translation_bald
-
-    # Check if there is enough translations
-    if hc != 4:  # not bald
-        assert len(translations) == 24, (
-            f'The number of translations is {len(translations)} while it '
-            'should be 24.'
-            )
-    else:  # bald
-        assert len(translations) == 25, (
-            f'The number of translations is {len(translations)} while it '
-            'should be 25 (for bald people).'
-            )
+    if caracs is not None:
+        # Replace 'Hc_4' by 'bald':
+        if hc != 4:
+            translation_bald = translations.pop('Hc_4')
+            translations['bald'] = translation_bald
 
     return translations
