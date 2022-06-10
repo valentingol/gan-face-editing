86aea653eafb45f91eb244e98f7316568b76acff
Valentin Goldité <valentin.goldite@gmail.com>
2022-06-10 20:31:43 +0200

    🚧 trying to fix pylint worflow

--- .badge/pylint_get_color.py
+++ .badge/pylint_get_color.py
@@ -5,12 +5,12 @@ if __name__ == '__main__':
     arg = sys.argv[1]
     score = float(arg.split('=')[1])
     if score < 2:
-        print('#a30000')
+        print('"#a30000"')
     elif score < 4:
-        print('#a33400')
+        print('"#a33400"')
     elif score < 6:
-        print('#9ea300')
+        print('"#9ea300"')
     elif score < 8:
-        print('#6fa300')
+        print('"#6fa300"')
     else:
-        print('#36a300')
+        print('"#36a300"')
