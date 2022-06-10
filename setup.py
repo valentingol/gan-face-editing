2f0e86dd3b1d45fe6768cf649693c2b4374f7aba
Valentin Goldité <valentin.goldite@gmail.com>
2022-06-10 22:08:39 +0200

    🆙 version upped to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # Installation
 config = {
     'name': 'gan-face-editing',
-    'version': '1.0',
+    'version': '1.0.1',
     'description': 'Inter Centrales Ceteris Paribus face challenge.',
     'author': 'Valentin Goldite',
     'author_email': 'valentin.goldite@gmail.com',
