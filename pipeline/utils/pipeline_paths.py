29c533927f4913a5b5c6b6415c139f2b033d5768
Valentin Goldité <valentin.goldite@gmail.com>
2022-06-15 18:48:50 +0200

    :sparkles: Allow all postprocess operations to be skippable

---
+++ pipeline/utils/pipeline_paths.py
@@ -0,0 +1,95 @@
+"""Utilities for pipeline paths."""
+
+from os.path import join
+
+
+def get_pipeline_paths(config):
+    """Get the configs for the pipeline."""
+    save_intermediate = config.pipeline.save_intermediate
+    skip_domain_mixup = config.pipeline.skip_domain_mixup
+    skip_segmentation = config.pipeline.skip_segmentation
+    skip_depth_segmentation = config.pipeline.skip_depth_segmentation
+
+    projection_dir = config.projection_dir
+    result_dir = config.result_dir
+    config_flexible = config.anycost_config_flexible
+    anycost_config = ('anycost-ffhq-config-f-flexible' if config_flexible
+                      else 'anycost-ffhq-config-f')
+
+    pipeline_paths = {
+        'data_dir': config.data_dir,
+        'apply_translations': {
+            'projection_dir': projection_dir,
+            'output_path': join(result_dir, 'images_post_translation'),
+            'anycost_config': anycost_config,
+            'configs': config.translation,
+            },
+        'domain_mix': {
+            'domains_dist_path': 'postprocess/domain_mixup/distances',
+            'domains_img_path': 'postprocess/domain_mixup/domains',
+            'configs': config.domain_mixup,
+            },
+        'segmentation_mix': {
+            'model_path': 'postprocess/segmentation/model/79999_iter.pth',
+            'configs': config.segmentation,
+            },
+        'depth_estimation_mix': {
+            'model_path': ('postprocess/depth_segmentation/model/'
+                           'dpt_large-midas-2f21e586.pt'),
+            'configs': config.depth_segmentation,
+            },
+        }
+
+    op_order = ['apply_translations', 'domain_mix', 'segmentation_mix',
+                'depth_estimation_mix']
+
+    if skip_domain_mixup:
+        op_order.remove('domain_mix')
+        del pipeline_paths['domain_mix']
+    if skip_segmentation:
+        op_order.remove('segmentation_mix')
+        del pipeline_paths['segmentation_mix']
+    if skip_depth_segmentation:
+        op_order.remove('depth_estimation_mix')
+        del pipeline_paths['depth_estimation_mix']
+
+    pipeline_paths = set_paths(pipeline_paths, op_order, result_dir,
+                               save_intermediate)
+
+    return pipeline_paths
+
+
+def set_paths(pipeline_paths, op_order, result_dir, save_intermediate):
+    """Set input path and output path for each operation."""
+    for op_i, op_name in enumerate(op_order):
+        if op_i != 0:  # not apply_translations
+            if save_intermediate:
+                prev_op = op_order[op_i - 1]
+                for op_n, key_path in zip([prev_op, op_name],
+                                          ['input_path', 'output_path']):
+                    if op_n == 'apply_translations':
+                        pipeline_paths[op_name][key_path] = join(
+                            result_dir, 'images_post_translation'
+                            )
+                    if op_n == 'domain_mix':
+                        pipeline_paths[op_name][key_path] = join(
+                            result_dir, 'images_post_domain_mixup'
+                            )
+                    if op_n == 'segmentation_mix':
+                        pipeline_paths[op_name][key_path] = join(
+                            result_dir, 'images_post_segmentation'
+                            )
+                    if op_n == 'depth_estimation_mix':
+                        pipeline_paths[op_name][key_path] = join(
+                            result_dir, 'images_post_depth_segmentation'
+                            )
+            else:
+                pipeline_paths[op_name]['input_path'] = join(result_dir,
+                                                             'output_images')
+                pipeline_paths[op_name]['output_path'] = join(result_dir,
+                                                              'output_images')
+        if op_i == 0 and not save_intermediate:
+            pipeline_paths['apply_translations']['output_path'] = join(
+                result_dir, 'output_images'
+                )
+    return pipeline_paths
